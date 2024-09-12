c01946e243d3a65442eb4b18fd2fb39730609a4e
Madhav Jadav <102902932+jadavmadhavkumar@users.noreply.github.com>
2024-09-12 17:40:55 +0000

    second part

---
+++ data_type.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all  js code as newer version
+
+// alert(3+4)  not using in codespae 
+
+let name =  "Madhav"
+let age = 13 
+let isLoggedId =  false
+
+
+// number =>  2 to power 53 
+// digite 
+// string => ""
+// Boolean => true/ false
+// null =>  standalonr values 
+// undefined
+// Symbol => unique 
+
+// object 
+
+console.log(typeof "Madhav");
---
+++ varible.js
@@ -0,0 +1,15 @@
+const accountId =  144553
+let accountEmail  =  "Madhav.nfsu@gmail.com"
+var accountPassword =  "12344"
+accountCity =  "Vavdi"
+let accountState;
+
+
+// accountId =  2  not allowes 
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+
+console.table([accountEmail , accountId , accountPassword , accountCity , accountState])
+
