8c201611dc2eaaf81bdde4e35be802e245be8326
Madhav Jadav <102902932+jadavmadhavkumar@users.noreply.github.com>
2025-06-09 06:27:11 +0000

    number and Math

---
+++ Cai_aur_code/NUm_Maths/num_maths.js
@@ -0,0 +1,33 @@
+const num =  1000 
+console.log(num)
+
+
+const balance =  new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1));
+
+
+const otherNumber =  123.6738
+
+console.log(otherNumber.toPrecision(3))
+// 
+const  hundra  = 1000000 
+
+console.log(hundra.toLocaleString('en-IN'));
+console.log(hundra.toLocaleString());
+
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.ceil(4.2));
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
+const min = 10 
+const max  = 20 
+
+console.log(Math.floor(Math.random()  * (max - min +  1))+ min);
+
+
